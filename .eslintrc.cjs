4e73d5221d4013dc8c6704a35e2fb13d28008ed7
Diego Castillo <diego.castillo@tagwizz.com.mx>
2023-08-21 22:06:22 -0400

    :rotating_light: linter config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,22 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb-typescript/base",
+    "plugin:react-hooks/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', "vite.config.ts"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "react-hooks", "prettier", "react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "react/react-in-jsx-scope": "off",
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "react/jsx-curly-spacing": ["error", "never"],
     "indent": ["error", 2]
   },
 }
